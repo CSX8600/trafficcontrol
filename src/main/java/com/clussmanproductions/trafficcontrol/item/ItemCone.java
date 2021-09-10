e6b24195ad5836e70493f83f30c92237fa9f0389
RavenholmZombie <supersoftware1234@gmail.com>
2021-09-10 18:06:06 -0400

    Tooltip and Cone Hat

---
+++ src/main/java/com/clussmanproductions/trafficcontrol/item/ItemCone.java
@@ -0,0 +1,43 @@
+package com.clussmanproductions.trafficcontrol.item;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemBlock;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class ItemCone extends ItemBlock
+{
+    public ItemCone(Block block)
+    {
+        super(block);
+    }
+
+    @Override
+    public EntityEquipmentSlot getEquipmentSlot(ItemStack stack)
+    {
+        return EntityEquipmentSlot.HEAD;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
+    {
+        if(GuiScreen.isShiftKeyDown())
+        {
+            String info = I18n.format("tc.tt.cone");
+            tooltip.addAll(Minecraft.getMinecraft().fontRenderer.listFormattedStringToWidth(info, 150));
+        }
+        else
+        {
+            tooltip.add(TextFormatting.YELLOW + I18n.format("tc.tt.help"));
+        }
+    }
+}
