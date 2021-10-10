105e8bf35e240bfe2cd9a45ce45e3e78c55d0186
RavenholmZombie <supersoftware1234@gmail.com>
2021-10-10 16:27:08 -0400

    Added missing Client side designation
    wtf happened to it in the first place? :(

--- src/main/java/com/clussmanproductions/trafficcontrol/item/ItemCone.java
+++ src/main/java/com/clussmanproductions/trafficcontrol/item/ItemCone.java
@@ -11,6 +11,8 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
 import java.util.List;
@@ -29,6 +31,7 @@ public class ItemCone extends ItemBlock
     }
 
     @Override
+	@SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
         if(GuiScreen.isShiftKeyDown())
