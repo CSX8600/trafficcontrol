d7590514d14e9ba05cd30ec185081870ba1835b3
Gamearoo Codes <gamearoo10@gmail.com>
2021-12-16 12:53:54 -0500

    Update TrafficLight6Renderer.java
    Fixed it so bulbs match the location in the gui

--- src/main/java/com/clussmanproductions/trafficcontrol/tileentity/render/TrafficLight6Renderer.java
+++ src/main/java/com/clussmanproductions/trafficcontrol/tileentity/render/TrafficLight6Renderer.java
@@ -15,8 +15,9 @@ public class TrafficLight6Renderer extends BaseTrafficLightRenderer {
 	protected List<BulbRenderer> getBulbRenderers() {
 		return ImmutableList
 				.<BulbRenderer>builder()
-				.add(new BulbRenderer(12, 11, 0))
-				.add(new BulbRenderer(-2, 11, 1))
+				.add(new BulbRenderer(12, 11, 1))
+				.add(new BulbRenderer(-2, 11, 0)) 
+				
 				.add(new BulbRenderer(5, 2.5, 2))
 				.add(new BulbRenderer(5, -4, 3))
 				.build();
